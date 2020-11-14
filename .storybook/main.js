94323d44e9d0d7436ff50e3928ddf5312e5cf450
plantstoen <plantstoen@gmail.com>
2020-11-15 01:12:20 +0900

    test: some useful addon (Knobs, Action, Docs)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
   stories: [
     ".././src/stories/**/*.stories.mdx",
-    ".././src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+    ".././src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-knobs/register",
+    "@storybook/addon-docs",
   ],
 };
